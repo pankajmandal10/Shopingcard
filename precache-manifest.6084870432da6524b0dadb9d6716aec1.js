570abbad312261dbe8195bedd3873ee5dbbb5c86
pankajmandal1996 <mandalpankaj10696@gmail.com>
2021-03-07 11:33:35 +0530

    Updates

---
+++ precache-manifest.6084870432da6524b0dadb9d6716aec1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "773b6bd138f77cda3b142c06fc32708f",
+    "url": "/Shopingcard/index.html"
+  },
+  {
+    "revision": "5529a42cb2fbc367a13d",
+    "url": "/Shopingcard/static/css/2.83c7b7e9.chunk.css"
+  },
+  {
+    "revision": "e0d21e55703bc455fc82",
+    "url": "/Shopingcard/static/css/main.2926e1e8.chunk.css"
+  },
+  {
+    "revision": "5529a42cb2fbc367a13d",
+    "url": "/Shopingcard/static/js/2.ce528ff0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Shopingcard/static/js/2.ce528ff0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0d21e55703bc455fc82",
+    "url": "/Shopingcard/static/js/main.3f166d86.chunk.js"
+  },
+  {
+    "revision": "de07d5ca76ce22df630c",
+    "url": "/Shopingcard/static/js/runtime-main.1dba6005.js"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/Shopingcard/static/media/bars-solid.b3f49afd.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Shopingcard/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Shopingcard/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Shopingcard/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Shopingcard/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Shopingcard/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/Shopingcard/static/media/shopping-cart-solid.8bf1af37.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/Shopingcard/static/media/times-solid.2c5552a1.svg"
+  }
+]);
